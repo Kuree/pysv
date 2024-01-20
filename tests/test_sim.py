1af9d2cb9f8017f958035f4be08f870112292d1b
Keyi Zhang <keyi@cs.stanford.edu>
2024-01-20 14:03:37 -0800

    fix multi-dim array test case

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -193,8 +193,8 @@ def test_verilator_return_reference(get_vector_filename, temp):
 def test_verilator_array(get_vector_filename, temp):
     @sv(a=DataType.IntArray[2])
     def set_value(a):
-        print(a[2][1])
-        a[2][1] = 42
+        print(a[2, 1])
+        a[2, 1] = 42
 
     lib_path = compile_lib([set_value], cwd=temp)
     header_file = os.path.join(os.path.abspath(temp), "test_verilator_array.hh")
