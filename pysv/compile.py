e4eb7ea33a64c7d18428811d9f40bd4609cc8dc7
Keyi Zhang <keyi@cs.stanford.edu>
2026-05-06 22:59:26 -0700

    try to fix buildkite CI

--- pysv/compile.py
+++ pysv/compile.py
@@ -64,7 +64,7 @@ def compile_lib(func_defs, cwd, lib_name="pysv", pretty_print=True, release_buil
     # add header definition
     cmake_args.append("-DDPI_HEADER_DIR=" + vlstd_path)
     # tell cmake where to find python (in case it's not in the system path)
-    cmake_args.append("-DPython_EXECUTABLE:FILEPATH=" + sys.executable)
+    cmake_args.append("-DPYTHON_EXECUTABLE:FILEPATH=" + sys.executable)
     subprocess.check_call(["cmake"] + cmake_args + [".."],
                           cwd=build_dir)
     # built it!
